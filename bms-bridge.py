98b67a96020db985d309279e02b233ae2f187048
Izak Burger <isburger@gmail.com>
2016-12-09 00:13:23 +0200

    Kick the Multi into charge-only if the BMS asks us to stop discharging.
    Ref #3.

--- bms-bridge.py
+++ bms-bridge.py
@@ -26,6 +26,9 @@ def main():
         # Find all solar chargers
         solarchargers = find_services(bus, 'com.victronenergy.solarcharger.')
 
+        # Find all vebus devices
+        vebuses = find_services(bus, 'com.victronenergy.vebus.')
+
         # When bms service is not available, for whatever reason, the script
         # will crash and rely on daemontools to restart it. Result is that
         # the solar charger will automatically stop charging since it doesn't
@@ -33,6 +36,10 @@ def main():
         # BMS. 
         charge = bool(bus.get_object(args.bms,
             "/Info/MaxChargeCurrent").get_dbus_method('GetValue',
+            "com.victronenergy.BusItem")())
+
+        discharge = bool(bus.get_object(args.bms,
+            "/Info/MaxDischargeCurrent").get_dbus_method('GetValue',
             "com.victronenergy.BusItem")())
 
         print("updating {} solarchargers. Charge is {}".format(len(solarchargers), charge))
@@ -44,6 +51,21 @@ def main():
                 'SetValue', 'com.victronenergy.BusItem')
             ping()
             update(int(charge)*200)
+
+        print("updating {} vebus systems. Discharge is {}".format(len(vebuses), discharge))
+
+        for v in vebuses:
+            mode = int(bus.get_object(v, '/Mode').get_dbus_method(
+                    'GetValue', 'com.victronenergy.BusItem')())
+
+            # If off, leave it off.
+            if mode == 4:
+                print("{} is switched off, leaving it off".format(v))
+                continue
+
+            update = bus.get_object(v, '/Mode').get_dbus_method(
+                'SetValue', 'com.victronenergy.BusItem')
+            update(3 if discharge else 1)
 
         sleep(args.interval)
 
